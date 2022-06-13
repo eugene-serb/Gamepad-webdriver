adcde1b4c4de4d2aff1615b41c3378a9551923bd
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-06-13 15:18:35 +0300

    Rewrited Gamepad Master JS and HTML template. Added support of the Vibration Actuator. Improved drawing of gamepads.

--- js/gamepad.js
+++ js/gamepad.js
@@ -2,72 +2,17 @@
 /* GAMEPAD MASTER */
 /* -------------- */
 
-'use strict'
-
-/* ------- */
-/* GAMEPAD */
-/* ------- */
+'use strict';
 
 class Gamepad {
-    constructor(item) {
-        this.gamepad = item;
-        this.interval = setInterval(this.update, 1);
+    constructor(gamepad) {
+        this.gamepad = gamepad;
     };
-
     update = () => {
         let gamepads = navigator.getGamepads();
         this.gamepad = gamepads[this.gamepad.index];
-
-        this.gamepadHandler();
-    };
-    gamepadHandler = () => {};
-};
-
-/* --------------- */
-/* GAMEPAD EXAMPLE */
-/* --------------- */
-
-class ActiveGamepad extends Gamepad {
-    constructor(item, container) {
-        super(item);
-        this.container = document.querySelector(`.${container}`);
-    };
-
-    gamepadHandler = () => {
-        this.container.innerHTML = `
-                                    <h3>GamePad ${this.gamepad.index}</h3>
-                                    <span>${this.gamepad.id}</span>
-                                    <br />
-                                    <span class="${this.gamepad.buttons[0].value ? 'pressed' : ''}">Key A</span>
-                                    <span class="${this.gamepad.buttons[1].value ? 'pressed' : ''}">Key B</span>
-                                    <span class="${this.gamepad.buttons[2].value ? 'pressed' : ''}">Key X</span>
-                                    <span class="${this.gamepad.buttons[3].value ? 'pressed' : ''}">Key Y</span>
-                                    <br />
-                                    <span class="${this.gamepad.buttons[4].value ? 'pressed' : ''}">Key LB</span>
-                                    <span class="${this.gamepad.buttons[5].value ? 'pressed' : ''}">Key RB</span>
-                                    <span class="${this.gamepad.buttons[6].value ? 'pressed' : ''}">Key LT</span>
-                                    <span class="${this.gamepad.buttons[7].value ? 'pressed' : ''}">Key RT</span>
-                                    <br />
-                                    <span class="${this.gamepad.buttons[8].value ? 'pressed' : ''}">Key Back</span>
-                                    <span class="${this.gamepad.buttons[9].value ? 'pressed' : ''}">Key Start</span>
-                                    <span class="${this.gamepad.buttons[10].value ? 'pressed' : ''}">Key at Left Stick</span>
-                                    <span class="${this.gamepad.buttons[11].value ? 'pressed' : ''}">Key at Right Stick</span>
-                                    <br />
-                                    <span class="${this.gamepad.buttons[12].value ? 'pressed' : ''}">Key Forward</span>
-                                    <span class="${this.gamepad.buttons[13].value ? 'pressed' : ''}">Key Backward</span>
-                                    <span class="${this.gamepad.buttons[14].value ? 'pressed' : ''}">Key Left</span>
-                                    <span class="${this.gamepad.buttons[15].value ? 'pressed' : ''}">Key Right</span>
-                                    <br />
-                                    <span>Left Stick X: ${this.gamepad.axes[0] ? this.gamepad.axes[0] : 'missing'}</span>
-                                    <span>Left Stick Y: ${this.gamepad.axes[1] ? this.gamepad.axes[1] : 'missing'}</span>
-                                    <span>Right Stick X: ${this.gamepad.axes[2] ? this.gamepad.axes[2] : 'missing'}</span>
-                                    <span>Right Stick Y: ${this.gamepad.axes[3] ? this.gamepad.axes[3] : 'missing'}</span>`;
     };
 };
-
-/* -------------- */
-/* GAMEPAD DRIVER */
-/* -------------- */
 
 class GamepadMaster {
     constructor() {
@@ -75,15 +20,92 @@ class GamepadMaster {
     };
 
     init = () => {
+        this.#DOMs();
+        
         if (!this.checkGamepadSupport()) {
-            console.log(`This browser does not support of gamepads`);
+            console.log(`This browser does not support of gamepads.`);
+            this.$message.innerText = `This browser does not support of gamepads.`;
             return;
+        } else {
+            console.log(`Press any gamepad's button or connect new gamepad.`);
+            this.$message.innerText = `Press any gamepad's button or connect new gamepad.`;
         };
 
         this.gamepads = [];
-
-        this.#DOMs();
         this.#eventListeners();
+
+        this.interval = setInterval(this.eventLoop, 1);
+    };
+
+    eventLoop = () => {
+        this.update();
+        this.draw();
+        this.eventHandler();
+    };
+    update = () => {
+        if (this.gamepads.length > 0) {
+            this.$messageBox.setAttribute('hidden', 'hidden');
+            this.gamepads.forEach(item => {
+                item.update();
+            });
+        } else {
+            this.$messageBox.removeAttribute('hidden', 'hidden');
+        };
+    };
+    draw = () => {
+        this.$OUTPUT.innerHTML = '';
+        if (this.gamepads.length > 0) {
+            this.gamepads.forEach(item => {
+                this.$OUTPUT.innerHTML += `
+                <section class="output-gamepad">
+                    <h3>GamePad ${item.gamepad.index}</h3>
+                    <span>${item.gamepad.id}</span>
+                    <br />
+                    <span class="${item.gamepad.buttons[0].value ? 'pressed' : ''}">Key A</span>
+                    <span class="${item.gamepad.buttons[1].value ? 'pressed' : ''}">Key B</span>
+                    <span class="${item.gamepad.buttons[2].value ? 'pressed' : ''}">Key X</span>
+                    <span class="${item.gamepad.buttons[3].value ? 'pressed' : ''}">Key Y</span>
+                    <br />
+                    <span class="${item.gamepad.buttons[4].value ? 'pressed' : ''}">Key LB</span>
+                    <span class="${item.gamepad.buttons[5].value ? 'pressed' : ''}">Key RB</span>
+                    <span class="${item.gamepad.buttons[6].value ? 'pressed' : ''}">Key LT</span>
+                    <span class="${item.gamepad.buttons[7].value ? 'pressed' : ''}">Key RT</span>
+                    <br />
+                    <span class="${item.gamepad.buttons[8].value ? 'pressed' : ''}">Key Back</span>
+                    <span class="${item.gamepad.buttons[9].value ? 'pressed' : ''}">Key Start</span>
+                    <span class="${item.gamepad.buttons[10].value ? 'pressed' : ''}">Key at Left Stick</span>
+                    <span class="${item.gamepad.buttons[11].value ? 'pressed' : ''}">Key at Right Stick</span>
+                    <br />
+                    <span class="${item.gamepad.buttons[12].value ? 'pressed' : ''}">Key Forward</span>
+                    <span class="${item.gamepad.buttons[13].value ? 'pressed' : ''}">Key Backward</span>
+                    <span class="${item.gamepad.buttons[14].value ? 'pressed' : ''}">Key Left</span>
+                    <span class="${item.gamepad.buttons[15].value ? 'pressed' : ''}">Key Right</span>
+                    <br />
+                    <span>Left Stick X: ${item.gamepad.axes[0] ? item.gamepad.axes[0] : 'missing'}</span>
+                    <span>Left Stick Y: ${item.gamepad.axes[1] ? item.gamepad.axes[1] : 'missing'}</span>
+                    <span>Right Stick X: ${item.gamepad.axes[2] ? item.gamepad.axes[2] : 'missing'}</span>
+                    <span>Right Stick Y: ${item.gamepad.axes[3] ? item.gamepad.axes[3] : 'missing'}</span>
+                    <br />
+                    <span>Vibration Actuator: ${item.gamepad.vibrationActuator ? 'Available' : 'missing'}</span>
+                    <span>${item.gamepad.vibrationActuator ? 'Press LB + RB for testing Vibration Actuator' : ''}</span>
+                </section>`;
+            });
+        };
+    };
+    eventHandler = () => {
+        if (this.gamepads.length > 0) {
+            this.gamepads.forEach(item => {
+                if (item.gamepad.buttons[4].pressed === true &&
+                    item.gamepad.buttons[5].pressed === true) {
+                    item.gamepad.vibrationActuator.playEffect('dual-rumble', {
+                        startDelay: 0,
+                        duration: 500,
+                        weakMagnitude: 1.0,
+                        strongMagnitude: 1.0,
+                    });
+                };
+            });
+        };
     };
 
     checkGamepadSupport = () => {
@@ -92,26 +114,19 @@ class GamepadMaster {
 
     #DOMs = () => {
         this.$OUTPUT = document.querySelector('.output-wrapper');
+        this.$message = document.querySelector('#message');
+        this.$messageBox = document.querySelector('.gamepad-message');
     };
     #eventListeners = () => {
         window.addEventListener('gamepadconnected', (event) => {
-            let gamepadContainer = document.createElement('section');
-            gamepadContainer.classList.add(`output-gamepad-${event.gamepad.index}`);
-            this.$OUTPUT.appendChild(gamepadContainer);
-            this.gamepads.push(new ActiveGamepad(event.gamepad, `output-gamepad-${event.gamepad.index}`));
-            /*console.log(`A new gamepad has been connected at index ${event.gamepad.index}.`);*/
+            this.gamepads.push(new Gamepad(event.gamepad));
         });
         window.addEventListener('gamepaddisconnected', (event) => {
             this.gamepads.forEach((item, index) => {
                 if (item.gamepad.id === event.gamepad.id) {
-                    clearInterval(this.gamepads[index].interval);
                     this.gamepads.splice(index, 1);
                 };
             });
-            let gamepadContainer = document.querySelector(`.output-gamepad-${event.gamepad.index}`);
-            gamepadContainer.setAttribute('hidden', '');
-            gamepadContainer.classList.remove(`output-gamepad-${event.gamepad.index}`);
-            /*console.log(`The gamepad with index ${event.gamepad.index} has been disabled.`);*/
         });
     };
 };
